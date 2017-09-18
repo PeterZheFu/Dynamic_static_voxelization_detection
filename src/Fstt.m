9aa5fd7756e002e2ed25a395ffd68554042593ce
peterzhefu <PeterZheFu@users.noreply.github.com>
2017-09-18 05:13:13 -0700

    dir error

--- src/Fstt.m
+++ src/Fstt.m
@@ -16,7 +16,7 @@ st.dr.pts  = fullfile(st.dr.mdr, sprintf('velodyne_points//data'));   % director
 st.dr.img  = fullfile(st.dr.mdr, sprintf('image_%02d//data//', st.dr.cam));  % directory of color images
 st.dr.lbl  = fullfile(st.dr.mdr, sprintf('label_%02d', st.dr.cam));                      % directory of tracklet labels
 st.dr.oxt  = fullfile(st.dr.mdr, 'oxts');                                                % directory of pose
-st.dr.clb  = fullfile(st.dr.mdr, '2011_09_26_calib/2011_09_26');                                               % directory of calibration
+st.dr.clb  = fullfile(st.dr.mdr, '2011_09_26');                                               % directory of calibration
 st.dr.rec  = fullfile(st.dr.mdr, 'result', filesep);                                                % directory of record
 st.dr.nsq  = numel(dir(fullfile(st.dr.mdr, sprintf('image_%02d', st.dr.cam)))) - 2;      % total number of tracking sequences
 %% start and end frames
